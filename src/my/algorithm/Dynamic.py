eb10a55329c729e088da6f51feca62e1ff6a69d0
lixiaoming <lixiaoming@izuche.com>
2021-01-18 18:00:02 +0800

    01背包问题

--- src/my/algorithm/Dynamic.py
+++ src/my/algorithm/Dynamic.py
@@ -36,20 +36,50 @@ class Node:
         self.cost = cost
         self.value = value
 
+    def __str__(self):
+        return "%d-%d-%d " % (self.num, self.cost, self.value)
+
+    def __repr__(self):
+        return "%d-%d-%d " % (self.num, self.cost, self.value)
+
 
 class Ele:
-    def __init__(self, node, value):
-        self.node = node
+    def __init__(self, value):
         self.value = value
 
+    def __str__(self):
+        return "%d" % self.value
 
-def package01(*node, weight):
-    result = []
+    def __repr__(self):
+        return "%d" % self.value
+
+
+def package01(node, weight):
+    result = [[0 for i in range(len(node))] for j in range(weight)]
+    print(result)
     if weight == 0:
         return result
-    
+    for i in range(weight):
+        j = 0
+        while j < len(node):
+            lastV = 0
+            if j - 1 >= 0:
+                lastV = result[i][j - 1].value
+            value = lastV
+            k = i + 1
+            if k >= node[j].cost:
+                newV = node[j].value
+                if (k - node[j].cost >= 0) and j - 1 >= 0:
+                    newV = newV + result[k - node[j].cost][j - 1].value
+                if newV > lastV:
+                    value = newV
+            result[i][j] = Ele(value)
+            j = j + 1
+    print(result)
 
 
 if __name__ == '__main__':
-    print(upstairs(10))
-    print(upstairsLoop(10))
+    # print(upstairs(10))
+    # print(upstairsLoop(10))
+    a = [Node(1, 1, 15), Node(2, 3, 30), Node(3, 1, 20)]
+    package01(a, weight=4)
